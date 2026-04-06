2cef7a0c1cadaf8777427b6084d841481457860a
anacforcelli <ana.forcelli@lsitec.org.br>
2026-04-06 14:51:36 -0300

    Fix gitignore

--- build/mcuboot/zephyr/zephyr.dts.d
+++
@@ -1,7 +0,0 @@
-empty_file.o: /home/ana/arduino-puppy/zephyr/misc/empty_file.c \
- /home/ana/arduino-puppy/zephyr/boards/caninos/poodle-toy-v1/poodle-toy-v1.dts \
- /home/ana/arduino-puppy/zephyr/dts/riscv/caninos/puppy_v2.dtsi \
- /home/ana/arduino-puppy/zephyr/dts/riscv/caninos/puppy.dtsi \
- /home/ana/arduino-puppy/zephyr/include/zephyr/dt-bindings/gpio/gpio.h \
- /home/ana/arduino-puppy/zephyr/dts/common/mem.h \
- /home/ana/arduino-puppy/bootloader/mcuboot/boot/zephyr/boards/poodle-toy-v1_puppy_v2.overlay
